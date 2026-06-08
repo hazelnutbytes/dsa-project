d0449f529b1ebf5aa92bf1b2456d93fe3466534f
Hazel Lakshmana <hazellakshmana684@gmail.com>
2026-06-09 01:53:07 +0530

    added document storage using unordered_map

--- smart_search_system.cpp
+++ smart_search_system.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 struct Document {
@@ -7,6 +8,15 @@ struct Document {
     string content;
 };
 
+unordered_map<int, Document> docs;
+
 int main() {
+    Document d1 = {1, "hello world"};
+    Document d2 = {2, "c++ is fun"};
+    Document d3 = {3, "search engine project"};
+
+    docs[d1.id] = d1;
+    docs[d2.id] = d2;
+    docs[d3.id] = d3;
     return 0;
 }
